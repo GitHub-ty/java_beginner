2164a5ca4864c1337426e6ac2bbe3bd6ddf85cc6
田野 <337782859@qq.com>
2023-10-13 00:11:37 +0800

    feat: 基础变量

---
+++ stu2023_10_12_01/Demo02Variable.java
@@ -0,0 +1,12 @@
+public class Demo02Variable {
+    public static void main(String[] args) {
+// 创建一个变量
+// 格式 数据类型 变量每次
+        int num1 = 25;
+        System.out.println(num1);
+// 向变量中存入一个数据
+// 格式：变量名称 = 数据值
+        num1 = 10;
+        System.out.println(num1);
+    }
+}
